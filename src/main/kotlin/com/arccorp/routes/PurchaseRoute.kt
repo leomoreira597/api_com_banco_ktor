71753c6580646779d536868dbab44d2d1346434d
Leonardo Moreira <lmoreira597@gmail.com>
2023-06-06 22:15:21 -0300

    Base basica feita

--- src/main/kotlin/com/arccorp/routes/PurchaseRoute.kt
+++ src/main/kotlin/com/arccorp/routes/PurchaseRoute.kt
@@ -9,10 +9,11 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import migration.Purchase
 import models.PurchaseModel
-import org.jetbrains.exposed.sql.insertAndGetId
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.greaterEq
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
 
 fun Route.purchaseRouting() = runBlocking {
     route("/purchase") {
@@ -34,17 +35,15 @@ fun Route.purchaseRouting() = runBlocking {
                     Purchase.slice(Purchase.columns).select { Purchase.id eq id }.singleOrNull()
                         ?.let { Purchase.toPurchaseModel(it) }
                 }
-                if(purchase != null){
+                if (purchase != null) {
                     call.respond(status = HttpStatusCode.OK, purchase)
-                }
-                else{
+                } else {
                     call.respondText(
                         "Compra não encontrada",
                         status = HttpStatusCode.NotFound
                     )
                 }
-            }
-            else{
+            } else {
                 call.respondText(
                     "Id invalido",
                     status = HttpStatusCode.BadRequest
@@ -65,6 +64,54 @@ fun Route.purchaseRouting() = runBlocking {
                         "Produto Criado com Sucesso!! ${newPurchase.value}"
                     )
                 }
+            }
+        }
+        put("{id?}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id != null) {
+                val updatePurchase = call.receive<PurchaseModel>()
+                transaction {
+                    val purchase = Purchase.select { Purchase.id eq id }.singleOrNull()
+                    if (purchase != null) {
+                        Purchase.update({ Purchase.id eq id }) {
+                            it[value] = updatePurchase.value
+                            it[userId] = updatePurchase.userId
+                        }
+                        launch { call.respondText("Compra alterada com sucesso", status = HttpStatusCode.OK) }
+                    } else {
+                        launch { call.respondText("Compra não encontrada", status = HttpStatusCode.NotFound) }
+                    }
+                }
+            } else {
+                launch { call.respondText("Id invalido", status = HttpStatusCode.BadRequest) }
+            }
+        }
+        delete("{id?}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+
+            if (id != null) {
+                transaction {
+                    val purchase = Purchase.select { Purchase.id eq id }.singleOrNull()
+
+                    if (purchase != null) {
+                        val deletedCount = Purchase.deleteWhere {Purchase.id greaterEq id}
+
+                        if (deletedCount > 0) {
+                            launch{ call.respondText("Compra excluída com sucesso", status = HttpStatusCode.OK) }
+                        } else {
+                            launch{
+                                call.respondText(
+                                    "Falha ao excluir a compra",
+                                    status = HttpStatusCode.InternalServerError
+                                )
+                            }
+                        }
+                    } else {
+                        launch{ call.respondText("Compra não encontrada", status = HttpStatusCode.NotFound) }
+                    }
+                }
+            } else {
+                call.respondText("O Id não pode ser nulo!!!", status = HttpStatusCode.BadRequest)
             }
         }
     }
